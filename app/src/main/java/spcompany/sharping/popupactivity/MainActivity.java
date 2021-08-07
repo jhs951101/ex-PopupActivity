5abec56ab8ab6e2a7e7aa774a0ef5f3cd88e7d8e
jhs951101 <spg1101@naver.com>
2021-08-08 04:44:29 +0900

    m

---
+++ app/src/main/java/spcompany/sharping/popupactivity/MainActivity.java
@@ -0,0 +1,42 @@
+package spcompany.sharping.popupactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView txtResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtResult = (TextView)findViewById(R.id.txtResult);
+        getSupportActionBar().setTitle("Hello World!");
+    }
+
+    //버튼
+    public void mOnPopupClick(View v){
+        //데이터 담아서 팝업(액티비티) 호출
+        Intent intent = new Intent(this, PopupActivity.class);
+        intent.putExtra("open", "Hello SharPinG!");
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+            if (resultCode == RESULT_OK) {
+                //데이터 받기
+                String result = data.getStringExtra("close");
+                txtResult.setText(result);
+            }
+        }
+    }
+}
